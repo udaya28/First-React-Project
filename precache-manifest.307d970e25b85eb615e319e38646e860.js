04cc198b551cb9287b3968db3eb01c608fc6b3ce
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-10-06 10:30:47 +0530

    Updates

---
+++ precache-manifest.307d970e25b85eb615e319e38646e860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15921bfe66a82bea299074f13ddcb56",
+    "url": "/First-React-Project/index.html"
+  },
+  {
+    "revision": "6d60bd304f31b89ae856",
+    "url": "/First-React-Project/static/css/main.ceea2f6d.chunk.css"
+  },
+  {
+    "revision": "001f0fb8c375fd8a6d43",
+    "url": "/First-React-Project/static/js/2.86c41a82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/First-React-Project/static/js/2.86c41a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d60bd304f31b89ae856",
+    "url": "/First-React-Project/static/js/main.86622503.chunk.js"
+  },
+  {
+    "revision": "c1ca1c4a6a98b72a26d4",
+    "url": "/First-React-Project/static/js/runtime-main.57acc3ce.js"
+  }
+]);
